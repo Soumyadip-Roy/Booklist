c2c1f335f580c8c3bc8f2a567510ba093c359288
ankitgenius63 <72158253+ankitgenius63@users.noreply.github.com>
2022-10-29 03:13:23 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 
-//  Change nav style while scrolling
+//  Change nav style while scrolling // by ankit kumar singh
 window.addEventListener('scroll', ()=>{
     document.querySelector('nav').classList.toggle('window-scrolled', window.scrollY > 0);
 })
